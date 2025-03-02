5420005ce67f4990437ebf60ed3d13c0d5851274
Ifeanyi Emmanuel <ifeanyiemmanueljoseph@gmail.com>
2025-03-02 09:04:33 -0800

    page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import React from "react";
-import { AuthProvider } from "@/context/auth-context";
 import HeroHeader from "@/components/header";
 
 export default function Page() {
-	return (
-		<AuthProvider>
-			<HeroHeader />
-		</AuthProvider>
-	);
+	return <HeroHeader />;
 }
